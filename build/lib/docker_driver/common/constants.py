7269ff7394d05ac9819e8770bfcbe285bc48d17e
SerenaFeng <feng.xiaowei@zte.com.cn>
2018-03-14 19:45:58 +0800

    add .gitignore
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- build/lib/docker_driver/common/constants.py
+++
@@ -1,7 +0,0 @@
-import os.path
-
-
-DOVETAIL_ETC = './etc'
-TESTCASE_PATH = os.path.join(DOVETAIL_ETC, 'testcases')
-COMPLIANCE_PATH = os.path.join(DOVETAIL_ETC, 'compliance')
-TESTING_CONF = './testconf'
